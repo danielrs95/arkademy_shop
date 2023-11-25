5aa0bdc6b631ca54147076d62ec3214bc1613214
Daniel <danielrs9504@gmail.com>
2023-11-25 16:21:59 -0500

    spec products new: initial format

--- spec/views/products/new.html.erb_spec.rb
+++ spec/views/products/new.html.erb_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "products/new", type: :view do
+RSpec.describe 'products/new', type: :view do
   before(:each) do
-    assign(:product, Product.new())
+    assign(:product, Product.new)
   end
 
-  it "renders new product form" do
+  it 'renders new product form' do
     render
 
-    assert_select "form[action=?][method=?]", products_path, "post" do
+    assert_select 'form[action=?][method=?]', products_path, 'post' do
     end
   end
 end
