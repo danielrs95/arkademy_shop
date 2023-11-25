6aca044c2e89c70b173236cdffaed0d6d53dc4c9
Daniel <danielrs9504@gmail.com>
2023-11-25 16:31:05 -0500

    spec/products/index: initial format

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "products/index", type: :view do
+RSpec.describe 'products/index', type: :view do
   before(:each) do
     assign(:products, [
-      Product.create!(),
-      Product.create!()
-    ])
+             Product.create!,
+             Product.create!
+           ])
   end
 
-  it "renders a list of products" do
+  it 'renders a list of products' do
     render
   end
 end
