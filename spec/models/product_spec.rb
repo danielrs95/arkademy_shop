9f08fd6a6b08e52f75dd54580d05d078e94eca37
Daniel <danielrs9504@gmail.com>
2023-11-25 20:42:16 -0500

    spec/models/product: test for missing params when saving

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  context 'when data is provided' do
+  context 'when data provided is valid' do
     let(:params) { { name: 'Sneakers', price: 49.99, quantity: 4 } }
 
     it 'saves the instance' do
@@ -13,5 +13,15 @@ RSpec.describe Product, type: :model do
         described_class.new(params).save!
       end.to change { Product.all.size }.by(1)
     }
+  end
+
+  context 'when data provided is invalid' do
+    let(:params) { { price: 49.99, quantity: 4 } }
+
+    it 'raises an ActiveRecord::RecordInvalid exception' do
+      expect do
+        described_class.new(params).save!
+      end.to raise_error(ActiveRecord::RecordInvalid)
+    end
   end
 end
