67860841605297696ae8e28a29a4bf4a2b183c40
Daniel <danielrs9504@gmail.com>
2023-11-26 20:54:18 -0500

    spec/requests: fix tests after changing user products permissions

--- spec/views/products/show.html.erb_spec.rb
+++ spec/views/products/show.html.erb_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "products/show", type: :view do
+RSpec.describe 'products/show', type: :view do
+  let(:valid_attributes) { { name: 'Sneakers', price: 49.99, quantity: 4 } }
+
   before(:each) do
-    @product = assign(:product, Product.create!())
+    @product = assign(:product, Product.create!(valid_attributes))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
   end
 end
