119377361f6cd344cec4c46e1aa6c37e650dd31f
Daniel <danielrs9504@gmail.com>
2023-11-26 21:16:54 -0500

    Add new tests for admin

--- spec/views/admin/products/edit.html.erb_spec.rb
+++ spec/views/admin/products/edit.html.erb_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "products/edit", type: :view do
+RSpec.describe "admin/products/edit", type: :view do
   before(:each) do
-    @product = assign(:product, Product.create!())
+    @product = assign(:product, Product.create!({ name: 'Sneakers', price: 49.99, quantity: 4 }))
   end
 
   it "renders the edit product form" do
